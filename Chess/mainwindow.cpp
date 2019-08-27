738b67dba74e3486eb76f2a10b6d4168c173506d
wmhst7 <wmhst@qq.com>
2019-08-27 09:50:55 +0800

    123

--- Chess/mainwindow.cpp
+++ Chess/mainwindow.cpp
@@ -1,14 +1,51 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPixmap>
+#include <QPainter>
+#define DISTANCE 85
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //设置背景图片
+    setAutoFillBackground(true);
+    QPalette palette;
+    QPixmap pixmap(":/Res_/ChessPan.jpg");
+    QPixmap pmap = pixmap.scaled(800,800,Qt::KeepAspectRatio);
+    palette.setBrush(backgroundRole(),QBrush(pmap));
+    this->setPalette(palette);
+
+
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::paintEvent(QPaintEvent* event){
+     qDebug()<<"Painting";
+     QPainter p(this);
+     p.setBrush(QColor("yellow"));
+     p.drawRect(85,85,10,10);
+     p.drawRect(715,85,10,10);
+     p.drawRect(715,715,10,10);
+     p.drawRect(85,715,10,10);
+
+}
+
+
+
+
+
+
+
+
+
+
+
--- Chess/mainwindow.h
+++ Chess/mainwindow.h
@@ -2,6 +2,29 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QObject>
+#include <QVector>
+#include <QString>
+#include <QStringList>
+#include <QMessageBox>
+#include <QPoint>
+#include <QDebug>
+
+#include <QFileDialog>
+
+#include <QTimer>
+
+#include <QLabel>
+#include <QProgressBar>
+#include <QTextBrowser>
+
+#include <QMouseEvent>
+#include<QKeyEvent>
+#include<QEvent>
+
+#include <QRegExp>
+#include <QSound>
+
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +40,8 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    void paintEvent(QPaintEvent*);
 };
 
 #endif // MAINWINDOW_H
